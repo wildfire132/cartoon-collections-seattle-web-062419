a3e5c9b84686ae68fbd5da586d586ef3d2a9f01a
Katana Tran <tkatyk21@gmail.com>
2019-06-20 20:41:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |item|
+    puts
 end
 
 def summon_captain_planet# code an argument here
