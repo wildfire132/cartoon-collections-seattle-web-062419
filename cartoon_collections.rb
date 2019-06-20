db1a084ebbd91531be0fb0794e33f752f23f7d9c
Katana Tran <tkatyk21@gmail.com>
2019-06-20 21:15:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(array)
   end 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
+  
 end
 
 def find_the_cheese# code an argument here
