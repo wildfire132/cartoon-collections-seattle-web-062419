b7e3b0e731b71d9fbc5711e22ec452b19abd92c5
Katana Tran <tkatyk21@gmail.com>
2019-06-20 21:24:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? do |counter|
-    if counter
-  
+  array.any? {|word| word.length >4}
 end
 
 def find_the_cheese# code an argument here
