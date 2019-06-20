68738a9f42ee7dd4ca41b27ed87f49479db60644
Katana Tran <tkatyk21@gmail.com>
2019-06-20 20:50:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,1 +1,16 @@
-c
+def roll_call_dwarves(array)
+  array.each_with_index do {|number, item| puts number item}
+end
+
+def summon_captain_planet# code an argument here
+  # Your code here
+end
+
+def long_planeteer_calls# code an argument here
+  # Your code here
+end
+
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
